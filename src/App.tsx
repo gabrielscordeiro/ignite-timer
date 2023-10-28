dc8605b3ecd00be1d1258b635c08f843acd0a855
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2023-10-27 21:36:45 -0300

    style: 💄 Global Style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { ThemeProvider } from 'styled-components'
+
 import { defaultTheme } from "./styles/themes/default.ts";
+import { GlobalStyle } from "./styles/globals.ts";
 
 function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
         Ignite Timer
+        <GlobalStyle />
     </ThemeProvider>
   )
 }
