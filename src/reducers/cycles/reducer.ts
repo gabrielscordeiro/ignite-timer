9ea414370f40d5979d1713e4f6aeb198758e8cc3
Gabriel Schmidt Cordeiro <gabrielscordeiro2012@gmail.com>
2023-11-08 21:50:56 -0300

    fix: 🐛 Fixing error in countdown

--- src/reducers/cycles/reducer.ts
+++ src/reducers/cycles/reducer.ts
@@ -1,5 +1,5 @@
-import { ActionTypes } from './actions.ts'
 import { produce } from 'immer'
+import { ActionTypes } from './actions'
 
 export interface Cycle {
   id: string
@@ -20,37 +20,37 @@ export function cyclesReducer(state: CyclesState, action: any) {
     case ActionTypes.ADD_NEW_CYCLE:
       return produce(state, (draft) => {
         draft.cycles.push(action.payload.newCycle)
-        draft.activeCycleId = action.payload.activeCycleId
+        draft.activeCycleId = action.payload.newCycle.id
       })
+
     case ActionTypes.INTERRUPT_CURRENT_CYCLE: {
-      const currentCycleIndex = state.cycles.findIndex(
-        (cycle) => cycle.id === state.activeCycleId,
-      )
+      const currentCycleIndex = state.cycles.findIndex((cycle) => {
+        return cycle.id === state.activeCycleId
+      })
 
       if (currentCycleIndex < 0) {
         return state
       }
 
       return produce(state, (draft) => {
-        draft.activeCycleId = null
         draft.cycles[currentCycleIndex].interruptedDate = new Date()
+        draft.activeCycleId = null
       })
     }
     case ActionTypes.MARK_CURRENT_CYCLE_AS_FINISHED: {
-      const currentCycleIndex = state.cycles.findIndex(
-        (cycle) => cycle.id === state.activeCycleId,
-      )
+      const currentCycleIndex = state.cycles.findIndex((cycle) => {
+        return cycle.id === state.activeCycleId
+      })
 
       if (currentCycleIndex < 0) {
         return state
       }
 
       return produce(state, (draft) => {
-        draft.activeCycleId = null
         draft.cycles[currentCycleIndex].finishedDate = new Date()
+        draft.activeCycleId = null
       })
     }
-
     default:
       return state
   }
